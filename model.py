3076435113716d18fb3459dc7844a72c9773e8ec
praneeth116 <praneeth.n116@gmail.com>
2025-02-26 00:48:21 +0530

    add MultiHeadAttention

--- model.py
+++ model.py
@@ -2,10 +2,6 @@ import torch
 import torch.nn as nn
 import math
 
-d_model = 10
-div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0)/d_model))
-print(div_term)
-
 class InputEmbedding(nn.Module): ## Converts a token into 512 dimensional vector
     def __init__(self, d_model: int, vocab_size: int):
         super().__init__()
@@ -17,29 +13,98 @@ class InputEmbedding(nn.Module): ## Converts a token into 512 dimensional vector
         return self.embedding(x)*math.sqrt(self.d_model)
     
 class PositionEncoding(nn.Module): ## For giving the information about each token in the sentence.
-        def __init__(self, d_model: int, seq_len: int, dropout: float):
-            # seq_len -> maximum input length of the sequence
-            # dropout -> to make the model less overfit
-            super().__init__()
-            self.d_model = d_model
-            self.dropout = nn.Dropout(dropout)
-            self.seq_len = seq_len
+    def __init__(self, d_model: int, seq_len: int, dropout: float):
+        # seq_len -> maximum input length of the sequence
+        # dropout -> to make the model less overfit
+        super().__init__()
+        self.d_model = d_model
+        self.dropout = nn.Dropout(dropout)
+        self.seq_len = seq_len
 
-            # Create a matrix of shape (seq_len , d_model)
-            pe = torch.zeros(seq_len, d_model)
-            # Create a vector of shape (seq_len, 1)
-            position = torch.arange(0,seq_len, dtype=torch.float).unsqueeze(1)
-            div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0)/d_model))
-            # Applying sin to even positions and cos to odd positions
-            pe[:, 0::2] = torch.sin(position * div_term)
-            pe[:, 1::2] = torch.cos(position * div_term)
+        # Create a matrix of shape (seq_len , d_model)
+        pe = torch.zeros(seq_len, d_model)
+        # Create a vector of shape (seq_len, 1)
+        position = torch.arange(0,seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0)/d_model))
+        # Applying sin to even positions and cos to odd positions
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
 
-            pe = pe.unsqueeze(0) #(1, seq_len, d_model) as we have batches
+        pe = pe.unsqueeze(0) #(1, seq_len, d_model) as we have batches
             
-            '''buffers are tensors that are saved in the model's state but are not updated during training
-            (i.e., they are not considered model parameters and do not have gradients).'''
-            self.register_buffer('pe',pe)
+        '''buffers are tensors that are saved in the model's state but are not updated during training
+        (i.e., they are not considered model parameters and do not have gradients).'''
+        self.register_buffer('pe',pe)
 
-        def forward(self, x):
-            x = x + (self.pe[:, :x.shape[1], :]).requires_grad(False) #As we don't want the positional encodings to change during training.
-            return self.dropout(x)
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad(False) #As we don't want the positional encodings to change during training.
+        return self.dropout(x)
+        
+class LayerNormalization(nn.Module):
+    def __init__(self, eps: float = 10**-6):
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1)) # Multiplied
+        self.beta = nn.Parameter(torch.zeros(1)) # Added
+    
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim = True)
+        std = x.std(dim=-1, keepdim = True)
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
+    
+class FeedForwardBlock(nn.Module):
+    def __init__(self, d_model: int, d_ff: int, dropout: float):
+        super().__init__()
+        self.layer1 = nn.Linear(d_model, d_ff)
+        self.droput = nn.Dropout(dropout)
+        self.layer2 = nn.Linear(d_ff, d_model)
+
+    def forward(self, x):
+        return self.layer2(self.droput(torch.relu(self.layer1(x))))
+    
+class MultiHeadAttentionBlock(nn.Module):
+    def __init__(self, d_model: int, h: int, dropout: float):
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+        assert d_model % h == 0, "d_model is not divisible by h"
+
+        self.d_k = d_model // h
+        self.w_q = nn.Linear(d_model, d_model)
+        self.w_k = nn.Linear(d_model, d_model)
+        self.w_v = nn.Linear(d_model, d_model)
+
+        self.w_o = nn.Linear(d_model, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+
+        attention_scores = (query @ key.transpose(-2,-1)) / math.sqrt(d_k)
+
+        if mask is not None:
+            attention_scores.masked_fill(mask==0, -1e9)
+        attention_scores = attention_scores.softmax(dim = -1) #(B, h, seq, seq)
+
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
+
+        return (attention_scores @ value), attention_scores
+
+    def forward(self, q, k, v, mask):
+        query = self.w_q(q) # (Batch, seq_len, d_model) -> (Batch, seq_len, d_model)
+        key = self.w_k(k)
+        value = self.w_v(v)
+
+        # (B, seq, d_model) -> (B, seq, h, d_k) -> (B, h, seq, d_k)
+        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1,2)
+        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1,2)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1,2)
+
+        x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
+
+        x = x.transpose(1,2) # (B, h, seq, d_k) -> (B, seq, h, d_k)
+        x = x.contiguous().view(x.shape[0], -1, self.h*self.d_k) # (B, seq, h, d_k) -> (B, seq, d_model)
+
+        return self.w_o(x) #(B, seq, d_model)
